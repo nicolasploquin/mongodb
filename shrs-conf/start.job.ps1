bcc4fe121ba8016f7b64b5d86b725936683ebd78
Nicolas PLOQUIN <nicolas.ploquin@gmail.com>
2019-03-26 10:48:36 +0100

    Ajout des commandes mkdir pour dossier data

--- shrs-conf/start.job.ps1
+++ shrs-conf/start.job.ps1
@@ -1,14 +1,27 @@
+$shardsPath = "C:\_applications\mongodb\shrs2"
+$confPath = "C:\_formations\_mongdb\formation-2019-03\shrs-conf"
 
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\config1.conf" }
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\config2.conf" }
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\config3.conf" }
+New-Item -ItemType Directory -Path "$shardsPath\log"
+New-Item -ItemType Directory -Path "$shardsPath\config\1"
+New-Item -ItemType Directory -Path "$shardsPath\config\2"
+New-Item -ItemType Directory -Path "$shardsPath\config\3"
+New-Item -ItemType Directory -Path "$shardsPath\shardA\1"
+New-Item -ItemType Directory -Path "$shardsPath\shardA\2"
+New-Item -ItemType Directory -Path "$shardsPath\shardA\3"
+New-Item -ItemType Directory -Path "$shardsPath\shardB\1"
+New-Item -ItemType Directory -Path "$shardsPath\shardB\2"
+New-Item -ItemType Directory -Path "$shardsPath\shardB\3"
 
-Start-Job -ScriptBlock { mongos.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\router.conf" }
+for ($i = 1; $i -le 3; $i++) {
+    Start-Job -ScriptBlock { mongod.exe --config "$confPath\config$i.conf" }
+}
 
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\shardA1.conf" }
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\shardA2.conf" }
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\shardA3.conf" }
+Start-Job -ScriptBlock { mongos.exe --config "$confPath\router.conf" }
 
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\shardB1.conf" }
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\shardB2.conf" }
-Start-Job -ScriptBlock { mongod.exe --config "C:\_formations\_mongdb\formation-2019-03\shrs-conf\shardB3.conf" }
+Start-Job -ScriptBlock { mongod.exe --config "$confPath\shardA1.conf" }
+Start-Job -ScriptBlock { mongod.exe --config "$confPath\shardA2.conf" }
+Start-Job -ScriptBlock { mongod.exe --config "$confPath\shardA3.conf" }
+
+Start-Job -ScriptBlock { mongod.exe --config "$confPath\shardB1.conf" }
+Start-Job -ScriptBlock { mongod.exe --config "$confPath\shardB2.conf" }
+Start-Job -ScriptBlock { mongod.exe --config "$confPath\shardB3.conf" }
